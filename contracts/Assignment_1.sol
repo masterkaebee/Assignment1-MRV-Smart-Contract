66ca84d53d388558d4717fb7eca4a006528d1736
Karabelo <johnkxmohapi@gmail.com>
2026-08-18 13:22:06 +0200

    Initial commit - Facility Struct

---
+++ contracts/Assignment_1.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract MRV_Solution{
+
+    //Facility Struct
+    struct Facility {
+        uint facility_id;
+        string facility_name;
+        uint registered_by;
+        
+
+    }
+}
