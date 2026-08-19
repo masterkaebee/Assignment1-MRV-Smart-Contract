cb250b3db06be5ded54d0ab310c50b2b38ad8324
Karabelo <johnkxmohapi@gmail.com>
2026-08-19 14:15:17 +0200

    Adj. structs, added state vars

--- contracts/Assignment_1.sol
+++ contracts/Assignment_1.sol
@@ -2,13 +2,47 @@
 pragma solidity ^0.8.0;
 
 contract MRV_Solution{
+    
+    enum Outcome {Pending, Verified, Disputed, Rejected}
 
     //Facility Struct
     struct Facility {
-        uint facility_id;
-        string facility_name;
-        uint registered_by;
-        
-
+        uint facility_id; // facitlity identifier
+        string facility_name; // actual facility name - included for ease of reference
+        address registered_by; // important for audit trail/tracking - using address
+        bool is_active; // is the facility still operational
     }
+    
+    // Emissions Record Struct
+    struct EmissionsRecord{
+        uint record_id;
+        uint facility_id; // facitlity identifier
+        bytes32 data_hash; // hash of emmissions data - hashed for security
+        address submitted_by; // important for audit trail and tracking - using address
+        uint submitted_when; // submission timestamp - but using block.timestamp
+        
+    }
+
+    // Verification Struct
+    struct VerificationDetails {
+        uint record_id; // emissions record identifier
+        uint audit_firm_id; // audit firm identifier
+        uint audit_time; // audit timestamp - using block.timestamp
+        Outcome outcome;
+        bytes32 comments_hash; // hashed audit comments 
+    }
+
+    // State Variables
+    address public admin;
+    mapping(address => bool) public regulators;
+    mapping(address => bool) public personnel;
+    mapping(address => bool) public auditors; 
+    Facility[] public facilities;
+    EmissionsRecord[] public emission_records;
+    mapping(uint => string) public audit_firm_name;
+    mapping(address => uint) public auditor_to_firm;
+    mapping(uint => VerificationDetails[]) public record_verifications;
+
+
+    
 }
